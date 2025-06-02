a79717e92026205f4e2fa5c0b4630e38dcd50880
lertg1 <lertg1@gmail.com>
2025-06-02 20:37:39 +0800

    first build

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,41 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        return bookRepository.findById(id).map(book -> {
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setPrice(bookDetails.getPrice());
+            return bookRepository.save(book);
+        }).orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
